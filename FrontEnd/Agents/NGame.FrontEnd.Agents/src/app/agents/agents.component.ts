66e226345c9caf65c02ec40ec453de90824d785f
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2017-06-13 14:16:54 -0600

    Adding polymer component

---
+++ FrontEnd/Agents/NGame.FrontEnd.Agents/src/app/agents/agents.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agents',
+  templateUrl: './agents.component.html',
+  styleUrls: ['./agents.component.css']
+})
+export class AgentsComponent implements OnInit {
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- FrontEnd/Agents/NGame.FrontEnd.Agents/src/app/app.module.ts
+++ FrontEnd/Agents/NGame.FrontEnd.Agents/src/app/app.module.ts
@@ -1,18 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdToolbarModule, MdButtonModule, MdInputModule, MdSelectModule, MdCardModule, MdCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AgentsComponent } from './agents/agents.component';
+import { PlayersComponent } from './players/players.component';
+import { MarketsComponent } from './markets/markets.component';
+import { ReportsComponent } from './reports/reports.component';
+import { RulesComponent } from './rules/rules.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'agents', component: AgentsComponent },
+  { path: 'players', component: PlayersComponent },
+  { path: 'markets', component: MarketsComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'rules', component: RulesComponent },
+  { path: '', redirectTo: '/agents', pathMatch: 'full' },
+  { path: '**', redirectTo: '/agents', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AgentsComponent,
+    PlayersComponent,
+    MarketsComponent,
+    ReportsComponent,
+    RulesComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlexLayoutModule,
+    MdToolbarModule,
+    MdButtonModule,
+    MdInputModule,
+    MdSelectModule,
+    MdCardModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
